837f29e8176845178b76c664261f604e2320e06d
Alina Lang <lav0903@mail.ru>
2025-12-30 22:22:00 +0300

    add count of words in title

--- viceversa/views.py
+++ viceversa/views.py
@@ -7,5 +7,7 @@ def home(request):
 def reverse(request):
     user_text = request.GET['usertext']
     reversed_text = user_text[::-1]
+    count_of_words = len(user_text.split())
     return render(request, 'reverse.html', {'usertext':user_text,
-                                            'reversedtext': reversed_text})
+                                            'reversedtext': reversed_text,
+                                            'countofwords': count_of_words})
